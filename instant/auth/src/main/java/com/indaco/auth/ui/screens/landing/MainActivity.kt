bbc8c770cfce20ae99748c469cddf1ec666d63d2
Stefan <indacostefan@gmail.com>
2021-10-15 09:23:33 -0700

    got instant app working

---
+++ instant/auth/src/main/java/com/indaco/auth/ui/screens/landing/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.indaco.auth.ui.screens.landing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.indaco.auth.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

8dc76df27308fbd28722edf7f0ae61b56e26728e
Stefan <indacostefan@gmail.com>
2022-10-05 15:13:52 -0700

    app actions tool sucks. it can't figure out how to build consistent previews but it works when only capability and intent are there

---
+++ app/src/main/java/com/indaco/samples/ui/main/BiiActivity.kt
@@ -0,0 +1,12 @@
+package com.indaco.samples.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.indaco.samples.R
+
+class BiiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bii)
+    }
+}
